bea583d9581e8857d70188693dec4045983928db
c3n9 <arseniy11041104@gmail.com>
2023-03-14 11:09:51 +0300

    login and list

---
+++ Terminal/Pages/PApprovalOfPasses.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Terminal.Model;
+
+namespace Terminal.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PApprovalOfPasses.xaml
+    /// </summary>
+    public partial class PApprovalOfPasses : Page
+    {
+        int indexEmployee;
+        public PApprovalOfPasses(int employee)
+        {
+            InitializeComponent();
+            Refresh();
+            CBStatus.ItemsSource = App.DB.PassStatus.ToList();
+            indexEmployee = employee;
+        }
+
+        private void BSave_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedGuest = DGGuests.SelectedItem as Pass;
+            if (selectedGuest == null) 
+            {
+                MessageBox.Show("Выберите гостя");
+                return;
+            }
+            selectedGuest.PassStatus = CBStatus.SelectedItem as PassStatus;
+            App.DB.SaveChanges();
+            this.Refresh();
+        }
+        private void Refresh()
+        {
+            DGGuests.ItemsSource = App.DB.Pass.Where(p => p.EmployeeId == indexEmployee).ToList();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
---
+++ Terminal/Pages/PLogin.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Terminal.Model;
+
+namespace Terminal.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PLogin.xaml
+    /// </summary>
+    public partial class PLogin : Page
+    {
+        public PLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var employee = App.DB.Employee.FirstOrDefault(c => c.Code == TBCodeEmployee.Text);
+            if (employee == null) 
+            {
+                MessageBox.Show("Неверный код");
+                return;
+            }
+            NavigationService.Navigate(new PApprovalOfPasses(employee.Id));
+        }
+    }
+}
