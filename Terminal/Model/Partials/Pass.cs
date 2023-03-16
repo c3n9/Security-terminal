392d6fa60dcccc7cc837ed08b262a330021f801d
c3n9 <arseniy11041104@gmail.com>
2023-03-16 18:34:58 +0300

    filtration all list

---
+++ Terminal/Model/Partials/Pass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Model
+{
+    public partial class Pass
+    {
+        public  string FullName
+        {
+            get
+            {
+                return $"{Surname} {Name} {Patromic}";
+            }
+        }
+
+        public string Passport
+        {
+            get
+            {
+                return $"{PassportSeria} {PassportNumber}";
+            }
+        }
+    }
+}
--- Terminal/Pages/PApprovalOfPasses.xaml.cs
+++ Terminal/Pages/PApprovalOfPasses.xaml.cs
@@ -27,18 +27,20 @@ namespace Terminal.Pages
         {
             InitializeComponent();
             contextEmployee = employee;
-            if(contextEmployee.DepartmentId == 6)
+            var statuses = App.DB.PassStatus.ToList();
+            statuses.Insert(0, new PassStatus() { Name = "All" });
+            CBStatus.ItemsSource = statuses.ToList();
+            CBStatus.SelectedIndex = 0;
+            CBStatusForFilter.ItemsSource = App.DB.PassStatus.ToList();
+            CBDepartment.ItemsSource = App.DB.Department.ToList();
+
+
+            if (contextEmployee.DepartmentId == 7)
             {
-                CBStatus.ItemsSource = App.DB.PassStatus.ToList();
-                CBStatusForFilter.ItemsSource = App.DB.PassStatus.ToList();
-                CBDepartment.ItemsSource = App.DB.Department.ToList();
-            }
-            if(contextEmployee.DepartmentId == 7)
-            {
-                CBDepartment.ItemsSource = App.DB.Department.ToList();
                 CBStatus.Visibility = Visibility.Collapsed;
                 BSave.Visibility = Visibility.Collapsed;
                 CBStatusForFilter.Visibility = Visibility.Collapsed;
+                TBSearch.Visibility = Visibility.Visible;
             }
             Refresh();
         }
@@ -46,7 +48,7 @@ namespace Terminal.Pages
         private void BSave_Click(object sender, RoutedEventArgs e)
         {
             var selectedGuest = DGGuests.SelectedItem as Pass;
-            if (selectedGuest == null) 
+            if (selectedGuest == null)
             {
                 MessageBox.Show("Выберите гостя");
                 return;
@@ -57,10 +59,23 @@ namespace Terminal.Pages
         }
         private void Refresh()
         {
+            var filtred = App.DB.Pass.ToList();
+            var selectedDepartment = CBDepartment.SelectedItem as Department;
+            var selectedStatus = CBStatusForFilter.SelectedItem as PassStatus;
+            var searchText = TBSearch.Text.ToLower();
+
             if (contextEmployee.DepartmentId == 7)
-                DGGuests.ItemsSource = App.DB.Pass.Where(g => g.PassStatus.Id == 2).ToList();
-            else
-                DGGuests.ItemsSource = App.DB.Pass.ToList();
+                filtred = filtred.Where(g => g.PassStatusId == 2).ToList();
+            if (selectedDepartment != null)
+                filtred = filtred.Where(f => f.Employee.DepartmentId == selectedDepartment.Id).ToList();
+            if (selectedStatus != null && selectedStatus.Id != 0)
+                filtred = filtred.Where(f => f.PassStatusId == selectedStatus.Id).ToList();
+            if (string.IsNullOrWhiteSpace(searchText) == false)
+            {
+                filtred = filtred.Where(f => f.FullName.ToLower().Contains(searchText) || f.Passport.Contains(searchText)).ToList();
+            }
+
+            DGGuests.ItemsSource = filtred.ToList();
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -70,24 +85,41 @@ namespace Terminal.Pages
 
         private void BFilter_Click(object sender, RoutedEventArgs e)
         {
-            var selectedDepartment = CBDepartment.SelectedItem as Department;
-            var selectedStatus = CBStatusForFilter.SelectedItem as PassStatus;
-            if (selectedDepartment != null && selectedStatus == null)
-            {
-                if (contextEmployee.DepartmentId == 7)
-                    DGGuests.ItemsSource = App.DB.Pass.Where(g => g.Employee.DepartmentId == selectedDepartment.Id && g.PassStatus.Id == 2).ToList();
-                else
-                    DGGuests.ItemsSource = App.DB.Pass.Where(g => g.Employee.DepartmentId == selectedDepartment.Id).ToList();
+            Refresh();
 
-            }
-            else if (selectedStatus !=null && selectedDepartment == null)
-            {
-                DGGuests.ItemsSource = App.DB.Pass.Where(g => g.PassStatus.Id == selectedStatus.Id).ToList();
-            }
-            else if (selectedDepartment != null && selectedStatus != null)
-            {
-                DGGuests.ItemsSource = App.DB.Pass.Where(g => g.Employee.DepartmentId == selectedDepartment.Id && g.PassStatus.Id == selectedStatus.Id).ToList();
-            }
+            //var selectedDepartment = CBDepartment.SelectedItem as Department;
+            //var selectedStatus = CBStatusForFilter.SelectedItem as PassStatus;
+            //if (selectedDepartment != null && selectedStatus == null)
+            //{
+            //    if (contextEmployee.DepartmentId == 7)
+            //        DGGuests.ItemsSource = App.DB.Pass.Where(g => g.Employee.DepartmentId == selectedDepartment.Id && g.PassStatus.Id == 2).ToList();
+            //    else
+            //        DGGuests.ItemsSource = App.DB.Pass.Where(g => g.Employee.DepartmentId == selectedDepartment.Id).ToList();
+
+            //}
+            //else if (selectedStatus != null && selectedDepartment == null)
+            //{
+            //    DGGuests.ItemsSource = App.DB.Pass.Where(g => g.PassStatus.Id == selectedStatus.Id).ToList();
+            //}
+            //else if (selectedDepartment != null && selectedStatus != null)
+            //{
+            //    DGGuests.ItemsSource = App.DB.Pass.Where(g => g.Employee.DepartmentId == selectedDepartment.Id && g.PassStatus.Id == selectedStatus.Id).ToList();
+            //}
+        }
+
+        private void CBDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void CBStatusForFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
         }
     }
 }
